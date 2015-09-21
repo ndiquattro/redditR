6df0639ba7eb2c1467f92050f315d3539da629fe
Nick DiQuattro <ndiquattro@gmail.com>
2015-09-21 12:47:09 -0700

    saftey commit

--- vignettes/test.R
+++ vignettes/test.R
@@ -2,15 +2,9 @@
 library(redditR)
 library(dplyr)
 library(ggplot2)
-# Set codes
-client <- '0JwI1aJyiAGQ8w'
-secret <- 't0EhztPdn67M3q_v7_py-x1wTtE'
-
-# login
-token <- login_reddit('box_plot', 'i!12G8qRAQH%mO1Rr1Pq', client, secret)
 
 # Analyze
-df <- post_grabber('http://www.reddit.com/r/nfl/', 10)
+df <- post_grabber('http://www.reddit.com/', 10)
 
 ggplot(df, aes(ups, num_comments)) +
   geom_point()+
